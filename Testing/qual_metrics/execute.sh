b348c02fd5d2cba7a8940375582aad3544325124
danielhmendoza01 <danielhmendoza01@gmail.com>
2023-07-06 10:44:30 -0700

    clean

--- Testing/qual_metrics/execute.sh
+++ Testing/qual_metrics/execute.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=ezTrim_qual-notPar
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=drainford@tgen.org
+#SBATCH --mail-user=dmendoza@tgen.org
 #SBATCH --ntasks=1
+#SBATCH --cpus-per-task=20
 #SBATCH --mem=60gb
 #SBATCH --time 24:00:00
-#SBATCH --output=/scratch/drainford/logs/ezTrim_qual-notPar.log
-#SBATCH --cpus-per-task=20
+#SBATCH --output=/scratch/dmendoza/logs/files/logs/ezTrim_qual-notPar_%j.log
 
 module load python
 
